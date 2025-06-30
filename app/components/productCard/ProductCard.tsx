0837cd39c64648570061bcd1d5bebb097e22f620
eduviana <eduardoviana83@gmail.com>
2025-06-30 20:38:44 -0300

    mejora visual en ProductCard

--- app/components/productCard/ProductCard.tsx
+++ app/components/productCard/ProductCard.tsx
@@ -12,29 +12,29 @@ export const ProductCard = ({ product }: ProductCardProps) => {
 
   return (
     <Link href={href} className="block">
-  <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden flex flex-col items-center transition hover:shadow-md">
-    <div className="relative w-full h-44 mb-4">
-      <Image
-        src={product.thumbnail!}
-        alt={product.title}
-        fill
-        className="object-contain z-10"
-        sizes="(max-width: 768px) 100vw, 33vw"
-      />
-      {/* Gradiente encima de la imagen */}
-      <div className="absolute inset-0 z-20 pointer-events-none bg-gradient-to-b from-white/0 to-gray-300/20" />
-    </div>
+      <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden flex flex-col items-center transition hover:shadow-md">
+        <div className="relative w-full h-56">
+          <Image
+            src={product.thumbnail!}
+            alt={product.title}
+            fill
+            className="object-contain z-10 py-6"
+            sizes="(max-width: 768px) 100vw, 33vw"
+          />
+          {/* Gradiente encima de la imagen */}
+          <div className="absolute inset-0 z-20 pointer-events-none bg-gradient-to-b from-white/0 to-gray-300/20" />
+        </div>
 
-    <div className="flex flex-col px-3">
-      <h3 className="text-sm text-text-tertiary font-light text-center mb-2 line-clamp-2 h-10">
-        {product.title}
-      </h3>
+        <div className="flex flex-col px-3 pt-4">
+          <h3 className="text-sm text-text-tertiary font-light text-center mb-2 line-clamp-2 h-10">
+            {product.title}
+          </h3>
 
-      <span className="text-text-primary text-[22px] font-semibold self-center">
-        {formatCurrency(product.price)}
-      </span>
-    </div>
-  </div>
-</Link>
+          <span className="text-text-primary text-[22px] font-semibold self-center">
+            {formatCurrency(product.price)}
+          </span>
+        </div>
+      </div>
+    </Link>
   );
 };
